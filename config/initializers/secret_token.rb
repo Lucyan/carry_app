23dd17ca159cc482d02a2515a3f31bff15429f37
Leonardo <leonardo.olivares@me.com>
2012-09-03 12:43:57 -0300

    Inicio de la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarryApp::Application.config.secret_token = 'df791b51fc498aca7514e309dd60e97961428e9c8e951548f1a5ac70dedda1d44bb0f22b810038b9e2928377d36535d792d9b30a7700f766c60ef7111daad810'
