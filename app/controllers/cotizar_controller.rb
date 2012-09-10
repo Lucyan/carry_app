cc297289a0d67f63fb0ac8d684652b5446284ce4
Leonardo <leonardo@desarrollo.(none)>
2012-09-10 20:31:01 -0300

    Cotiza WIP

--- app/controllers/cotizar_controller.rb
+++ app/controllers/cotizar_controller.rb
@@ -1,17 +1,18 @@
 class CotizarController < ApplicationController
-  	def nuevo
-  		@cotizacion = Cotiza.new
-  		@punto_origen = Punto.new
-  		@punto_destino = Punto.new
-  	end
+	def nuevo
+		@cotizacion = Cotiza.new
+		@punto_origen = Punto.new
+		@punto_destino = Punto.new
+    session[:puntos] = []
+	end
 
-  	def entregar_puntos
-  		@punto = Punto.new(params[:punto])
+	def entregar_puntos
+		session[:puntos].push Punto.new(params[:punto])
 
-  		render :json => @punto.to_json
+		render :json => session[:puntos].to_json
 
 		# respond_to do |format|
 		# 	format.json { render :json => @punto.to_json }
 		# end
-  	end
+	end
 end
